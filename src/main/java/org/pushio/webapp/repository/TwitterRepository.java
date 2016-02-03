0da9bdea9678579f8385e18a1a8b9952d8c2553e
zhaobaodong <190200560@qq.com>
2016-02-03 11:33:06 +0800

    Revert "第一次提交"
    This reverts commit 26bb2960729b91cd937968065edbaaaf31a9c745.

--- src/main/java/org/pushio/webapp/repository/TwitterRepository.java
+++
@@ -1,9 +0,0 @@
-package org.pushio.webapp.repository;
-
-import org.pushio.webapp.entity.Twitter;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface TwitterRepository extends PagingAndSortingRepository<Twitter, Long>, JpaSpecificationExecutor<Twitter>{
-
-}
